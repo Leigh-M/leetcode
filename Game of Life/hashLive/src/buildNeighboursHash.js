9d57d91258d7ca9cf9323ec7246919f8374e2b4f
Leigh Mathieson <leigh.mathieson@gmail.com>
2019-01-25 17:46:06 +0000

    func buildNeighbours
    Map of:  key: {count, coords} for all adjacent cells

--- Game of Life/hashLive/src/buildNeighboursHash.js
+++ Game of Life/hashLive/src/buildNeighboursHash.js
@@ -1,55 +1,54 @@
 /* eslint-env browser */
+// Leetcode instructions to return initial array hence intentionally mutating liveCells param:
+/* eslint-disable no-param-reassign */
+
 const hashKey = require('./hashKey');
 
-function checkKey(key, neighbours, coords) {
-  (neighbours.has(key)) ? neighbours.get(key).count++ : neighbours.set(key, {
-    count: 1, coords });
+function setKey(key, neighbours, coords) {
+  if (neighbours.has(key)) {
+    neighbours.get(key).count++;
+  } else {
+    neighbours.set(key, { count: 1, coords });
+  }
 }
 
 function buildNeighboursHash(liveCells, neighbours, rows, cols) {
-  liveCells.forEach((item) => {
+  liveCells.forEach((item, index) => {
     // find neighbours
     const x = item.coords[0];
-    const y = item.coords[0];
+    const y = item.coords[1];
     // ensure within display border
-    if (x - 1 >= 0 && y - 1 >= 0) {
-      const key = hashKey([x - 1, y - 1]);
-      checkKey(key, neighbours, item.coords);
-    }
-
-    if (x + 1 < rows && y + 1 < cols) {
-      const key = hashKey([x + 1, y + 1]);
-      checkKey(key, neighbours, item.coords);
-    }
-
-    if (x - 1 >= 0 && y + 1 < cols) {
-      const key = hashKey([x - 1, y + 1]);
-      checkKey(key, neighbours, item.coords);
-    }
-
-    if (x + 1 < rows && y - 1 >= 0) {
-      const key = hashKey([x + 1, y - 1]);
-      checkKey(key, neighbours, item.coords);
-    }
-
-    if (y - 1 >= 0) {
-      const key = hashKey([x, y - 1]);
-      checkKey(key, neighbours, item.coords);
-    }
-
     if (x - 1 >= 0) {
       const key = hashKey([x - 1, y]);
-      checkKey(key, neighbours, item.coords);
+      setKey(key, neighbours, [x - 1, y]);
     }
-
+    if (x - 1 >= 0 && y - 1 >= 0) {
+      const key = hashKey([x - 1, y - 1]);
+      setKey(key, neighbours, [x - 1, y - 1]);
+    }
+    if (y - 1 >= 0) {
+      const key = hashKey([x, y - 1]);
+      setKey(key, neighbours, [x, y - 1]);
+    }
+    if (x + 1 < rows && y - 1 >= 0) {
+      const key = hashKey([x + 1, y - 1]);
+      setKey(key, neighbours, [x + 1, y - 1]);
+    }
     if (x + 1 < rows) {
       const key = hashKey([x + 1, y]);
-      checkKey(key, neighbours, item.coords);
+      setKey(key, neighbours, [x + 1, y]);
     }
-
+    if (x + 1 < rows && y + 1 < cols) {
+      const key = hashKey([x + 1, y + 1]);
+      setKey(key, neighbours, [x + 1, y + 1]);
+    }
     if (y + 1 < cols) {
       const key = hashKey([x, y + 1]);
-      checkKey(key, neighbours, item.coords);
+      setKey(key, neighbours, [x, y + 1]);
+    }
+    if (x - 1 >= 0 && y + 1 < cols) {
+      const key = hashKey([x - 1, y + 1]);
+      setKey(key, neighbours, [x - 1, y + 1]);
     }
   });
   return neighbours;
