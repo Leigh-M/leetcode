95a2efa00d550b5bf052315530c138fed84a7c5a
Leigh Mathieson <leigh.mathieson@gmail.com>
2019-01-29 12:30:28 +0000

    write life() system tests

--- Game of Life/hashLive/test/test.js
+++ Game of Life/hashLive/test/test.js
@@ -3,110 +3,144 @@
 const { expect } = require('chai');
 const buildNeighboursHash = require('../src/buildNeighboursHash');
 const buildLiveHash = require('../src/buildLiveHash');
-const tests = require('./testDataForbuildNeighboursTestRunner');
+const { buildNeighboursTests, logicTests, lifeTests } = require('./testRunnerData');
+const {
+  life, survive, overCrowded, lonely,
+} = require('../src/life');
+const {
+  tag1, tag2, tag3, tag4,
+} = require('./taggedTemplates');
 
 // unit tests
-describe('buildNeighboursHash_size_test', function () {
-  tests.forEach(function (test) {
+// tagged templates for output formatting/ readability
+
+// total neighbouring cells Map size test
+describe('buildNeighboursHash size test', function () {
+  buildNeighboursTests.forEach(function (test) {
     const neighbours = new Map();
-    it(`should build a Map of unique adjacent cells, test checks size of adjacent cells Map from initial liveCells: ${test.liveCells} is: ${test.expectedMapLength}`, function () {
-      buildNeighboursHash(buildLiveHash(test.liveCells), neighbours, 200, 200);
-      expect(neighbours.size).to.equal(test.expectedMapLength);
+    it(tag1`Should build a Map of neighbours from input cells ${test.liveCells} expected size ${test.expectedMapLength}`, function () {
+      const neighs = buildNeighboursHash(buildLiveHash(test.liveCells), neighbours, 200, 200);
+      expect(neighs.size).to.equal(test.expectedMapLength);
     });
   });
 });
 
-describe('buildNeighboursHash_adjacent_count_test', function () {
-  tests.forEach(function (test) {
+// exact adjacent cells count test
+describe('buildNeighboursHash neighbouring cell key: count test', function () {
+  buildNeighboursTests.forEach(function (test) {
     const neighbours = new Map();
-    buildNeighboursHash(buildLiveHash(test.liveCells), neighbours, 200, 200);
-    describe('checks the Map: neighbours has {keys: count} as expected', function () {
-      tests.forEach(function () {
-        // Map of expected results pairs: {key, count}
-        const expectedAdjMap = new Map(test.adj);
-        it(`should check key: count as expected for ${test.adj}`, function () {
-          expectedAdjMap.forEach(function (item, index) {
-            expect(item).to.equal(neighbours.get(index).count);
-          });
-        });
+    const neighs = buildNeighboursHash(buildLiveHash(test.liveCells), neighbours, 200, 200);
+    test.adj.forEach((item) => {
+      it(tag2`should expect count of neighbouring cells to cell: ${item[0]} to be: ${item[1]}`, function () {
+        expect(neighs.get(item[0]).count).to.equal(item[1]);
       });
     });
   });
 });
 
-/* System tests:
-
-describe('Underpopulation: die if live cell has fewer than 2 live neighbouring cells', function () {
-  const tests = [
-    {
-      input: [[3, 0], [1, 1], [4, 1], [2, 2], [4, 3], [0, 4], [2, 4], [4, 4]],
-      expected: [[2, 1], [3, 1], [3, 2], [1, 3], [3, 4]],
-    },
-  ];
-  tests.forEach(function (test) {
-    const neighbours = new Map();
-    buildNeighbours(test.input, neighbours, 10, 10);
-    it('should kill cells, expected cells remain after next tick from input cells', function () {
-      const res = life(test.input, neighbours);
-      expect(arrayContainsArray(res, test.expected)).to.be.true; 
+// survive test
+describe('survive logic test, remain as live', function () {
+  logicTests.forEach(function (test) {
+    it(`should expect cell ${test.stillLive} exists to be true`, function () {
+      const liveCells = buildLiveHash(test.live);
+      const neighbours = new Map();
+      const neighs = buildNeighboursHash(liveCells, neighbours, 200, 200);
+      const testCell = test.stillLive;
+      const res = survive(testCell, liveCells, neighs);
+      const exists = res.live.has(testCell);
+      expect(exists).to.equal(true);
     });
   });
 });
 
-describe('Survive: continue living if 2-3 live neighbouring cells', function () {
-  const tests = [
-    {
-      input: [[0, 0], [3, 0], [0, 1], [1, 1], [2, 1], [2, 2], [4, 2], [0, 3], [1, 3], [3, 3],
-        [4, 3], [0, 4], [3, 4], [4, 4]],
-      expected: [[0, 0], [0, 1], [2, 1], [4, 2], [0, 3], [1, 3], [0, 4], [3, 4], [4, 4], [2, 0],
-        [1, 4], [2, 4]],
-    },
-  ];
-
-  tests.forEach(function (test) {
-    const neighbours = new Map();
-    buildNeighbours(test.input, neighbours, 5, 5);
-    it('should leave expected cells surviving on next tick from input', function () {
-      const res = life(test.input, neighbours);
-      expect(arrayContainsArray(res, test.expected)).to.be.true; 
+describe('survive logic test, delete from neighbouring', function () {
+  logicTests.forEach(function (test) {
+    it(`should remove survivor cell ${test.stillLive} from neighbours Map to enable correct count on remaining dead cells`, function () {
+      const liveCells = buildLiveHash(test.live);
+      const neighbours = new Map();
+      const neighs = buildNeighboursHash(liveCells, neighbours, 200, 200);
+      const testCell = test.stillLive;
+      const res = survive(testCell, liveCells, neighs);
+      const inMap = res.neighs.has(testCell);
+      expect(inMap).to.equal(false);
+    });
+  });
+});
+// overCrowded test
+describe('overCrowded logic test, delete from liveCells', function () {
+  logicTests.forEach(function (test) {
+    it(tag3`should remove cell ${test.diedOverCrowding} from liveCells ${test.overCrowd}`, function () {
+      const liveCells = buildLiveHash(test.live);
+      const neighbours = new Map();
+      const neighs = buildNeighboursHash(liveCells, neighbours, 200, 200);
+      const testCell = test.diedOverCrowding;
+      const res = overCrowded(testCell, liveCells, neighs);
+      const inMap = res.live.has(testCell);
+      expect(inMap).to.equal(false);
     });
   });
 });
 
-describe('Overpopulation: die if more than 3 live neighbouring cells', function () {
-  const tests = [
-    {
-      input: [[1, 0], [2, 0], [3, 0], [1, 1], [2, 1], [3, 1], [4, 1], [3, 2], [4, 2], [0, 3],
-        [1, 3], [2, 3], [3, 3], [4, 3], [1, 4], [2, 4]],
-      expected: [[1, 0], [4, 0], [1, 1], [0, 2], [0, 3], [4, 3], [0, 4]],
-    },
-  ];
-  tests.forEach(function (test) {
-    const neighbours = new Map();
-    buildNeighbours(test.input, neighbours, 5, 5);
-    it('should kill many cells leaving expected cells from input cells', function () {
-      const res = life(test.input, neighbours);
-      expect(arrayContainsArray(res, test.expected)).to.be.true; 
+describe('overCrowded logic test, delete from neighbours Map', function () {
+  logicTests.forEach(function (test) {
+    it(`should remove cell ${test.diedOverCrowding} from neighbouring cells Map`, function () {
+      const liveCells = buildLiveHash(test.live);
+      const neighbours = new Map();
+      const neighs = buildNeighboursHash(liveCells, neighbours, 200, 200);
+      const testCell = test.diedOverCrowding;
+      const res = overCrowded(testCell, liveCells, neighs);
+      const inMap = res.neighs.has(testCell);
+      expect(inMap).to.equal(false);
+    });
+  });
+});
+// loney test
+describe('lonely logic test, delete from liveCells', function () {
+  logicTests.forEach(function (test) {
+    it(tag3`should remove cell ${test.diedLoneliness} from liveCells ${test.lonelyCells}`, function () {
+      const liveCells = buildLiveHash(test.live);
+      const neighbours = new Map();
+      const neighs = buildNeighboursHash(liveCells, neighbours, 200, 200);
+      const testCell = test.diedLoneliness;
+      const res = lonely(testCell, liveCells, neighs);
+      const inMap = res.live.has(testCell);
+      expect(inMap).to.equal(false);
     });
   });
 });
 
-describe('Alive: any dead cell with exactly 3 live neighbouring cell becomes alive', function () {
-  const tests = [
-    {
-      input: [[0, 0], [1, 0], [3, 0], [4, 0], [1, 1], [4, 1], [2, 2], [0, 3], [1, 3], [2, 3],
-        [3, 4]],
-      expected: [[0, 0], [1, 0], [2, 0], [3, 0], [4, 0], [0, 1], [1, 1], [4, 1], [0, 2], [2, 2],
-        [3, 2], [1, 3], [2, 3], [3, 3], [1, 4], [2, 4]],
-    },
-  ];
-  tests.forEach(function (test) {
-    const neighbours = new Map();
-    buildNeighbours(test.input, neighbours, 5, 5);
-    it('should produce new cells as expected from input', function () {
-      const res = life(test.input, neighbours);
-      expect(arrayContainsArray(res, test.expected)).to.be.true; 
+describe('lonely logic test, delete from neighbours Map', function () {
+  logicTests.forEach(function (test) {
+    it(tag3`should remove cell ${test.diedLoneliness} from neighbouring cells Map with input cells ${test.lonelyCells}`, function () {
+      const liveCells = buildLiveHash(test.live);
+      const neighbours = new Map();
+      const neighs = buildNeighboursHash(liveCells, neighbours, 200, 200);
+      const testCell = test.diedLoneliness;
+      const res = lonely(testCell, liveCells, neighs);
+      const inMap = res.neighs.has(testCell);
+      expect(inMap).to.equal(false);
     });
   });
 });
-*/
+
+// life(), whole system test
+describe('life test', function () {
+  lifeTests.forEach(function (test) {
+    it(tag4`should expect liveCells Map after life() has returned with input cells ${test.seed} after next tick to contain all ${test.expected} and be same size`, function () {
+      const liveCells = buildLiveHash(test.seed);
+      const neighbours = new Map();
+      const neighs = buildNeighboursHash(liveCells, neighbours, 200, 200);
+      const nextCells = life(liveCells, neighs, 200, 200);
+      // used to build hashed Map of expected
+      const expected = buildLiveHash(test.expected);
+      // check expected Map contains every key in nextCells & size the same
+      let containsAll = true;
+      nextCells.forEach((item, index) => {
+        if (!expected.has(index)) {
+          containsAll = false;
+        }
+      });
+      expect(containsAll && (nextCells.size === expected.size)).to.equal(true);
+    });
+  });
+});
