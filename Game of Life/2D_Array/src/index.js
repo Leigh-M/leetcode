66fc09816e60f0bf8d1f4b627c5bb2f6a0e9a8c5
Leigh Mathieson <leigh.mathieson@gmail.com>
2019-01-29 18:30:45 +0000

    Acorn seed 200 * 200 in place of random grid

--- Game of Life/2D_Array/src/index.js
+++ Game of Life/2D_Array/src/index.js
@@ -9,16 +9,30 @@ function createGrid(rows, cols) {
   for (let i = 0; i < rows; i++) {
     grd[i] = [];
     for (let j = 0; j < cols; j++) {
-      grd[i][j] = Math.round(Math.random());
+      grd[i][j] = 0;
     }
   }
   return grd;
 }
 
-const grid = createGrid(100, 100);
+// create all zero's grid
+const initialGrid = createGrid(200, 200);
+
+// 'acorn' seed as 1's
+function setSeed(seed, targerArr) {
+  const grd = targerArr;
+  for (let i = 0; i < seed.length; i++) {
+    grd[seed[i][0]][seed[i][1]] = 1;
+  }
+  return grd;
+}
+
+// 'acorn' seed initial start
+const seed = [[100, 100], [101, 100], [101, 98], [103, 99], [104, 100], [105, 100], [106, 100]];
+const grid = setSeed(seed, initialGrid);
 
 function draw() {
-  ctx.clearRect(0, 0, 100, 100);
+  ctx.clearRect(0, 0, 200, 200);
   life(grid);
   ctx.fillStyle = '#101010';
   for (let i = 0; i < grid.length; i++) {
@@ -35,3 +49,20 @@ requestAnimationFrame(draw);
 
 // to test draw function
 module.exports = draw;
+
+
+/*
+// for a random grid of binary digits
+function createGrid(rows, cols) {
+  const grd = [];
+  for (let i = 0; i < rows; i++) {
+    grd[i] = [];
+    for (let j = 0; j < cols; j++) {
+      grd[i][j] = Math.round(Math.random());
+    }
+  }
+  return grd;
+}
+
+const grid = createGrid(100, 100);
+*/
